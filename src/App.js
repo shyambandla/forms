b2534d5ab1e690055fd81552d9346f3d59694528
Shyam bandla <shyambandla143@gmail.com>
2022-08-02 18:31:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
 
 function App() {
+
+
+const [name, setName] = useState('');
+const [password, setPassword] = useState('');
+
+
+
+const handleSubmit = (e) => {
+
+
+  e.preventDefault();
+
+  console.log(name, password);
+
+  const obj={
+    name: name,
+    age: password
+  }
+
+
+  // axios.get('http://localhost:5000/api/users/'+name).then(res => {
+  //   console.log(res.data);
+  // }).catch(err => {
+  //   console.log(err);
+  // });
+
+  axios.post('http://localhost:4000/addUser', obj).then(res => {
+    console.log(res.data);
+  }).catch(err => {
+    console.log(err);
+  });
+
+
+
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="background">
+      <div className="loginBox">
+        <form onSubmit={handleSubmit}>
+        <div>
+        <input type="text"  onChange={(e)=>{
+          setName(e.target.value)
+        }} maxLength={10} name='fullname' value={name} placeholder="Username" required/>
+        </div>
+        <div>
+        <input onChange={(e)=>{
+          setPassword(e.target.value)
+        }}  type="password" name='password' placeholder="Password.." required/>
+        </div>
+        <div>
+
+          <input type="submit" value="submit" />
+          </div>
+          </form>
+        </div>
+
     </div>
+    </>
   );
 }
 
